ad5e89aac90e61a6f85ac65fe2ad56800ef13887
Kazooooo <cest93@gmail.com>
2018-04-01 21:49:01 +0900

    index.tsのexport をまとめる

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,10 +3,15 @@ import * as functions from "firebase-functions";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const goodbye = functions.https.onRequest((request, response) => {
+const goodbye = functions.https.onRequest((request, response) => {
   response.send("Goodbye!");
 });
+
+export {
+  helloWorld,
+  goodbye,
+};
