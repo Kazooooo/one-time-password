b9f990347982d46b206e8e899afb9a87ddb191fd
Kazooooo <cest93@gmail.com>
2018-04-02 01:09:10 +0900

    createUserからphoneをuidとしてユーザーを作成

--- functions/src/createUser.ts
+++ functions/src/createUser.ts
@@ -1,7 +1,21 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
 const createUser = functions.https.onRequest((req, res) => {
-  res.send(req.body);
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    res.status(422).send({ error: "Bad Input" });
+  } else {
+    // Format the phone number to remove dashes and parens
+    const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+    // Create a new account using that phone number
+    admin.auth().createUser({ uid: phone })
+      .then((user) => res.send(user))
+      .catch((err) => res.status(422).send({ error: err }));
+
+    // Respond to the user request, saying the account was made
+  }
 });
 
 export default createUser;
