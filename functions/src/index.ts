7f022c02debb3caa6e0492fb85e106d272b0d5c9
Kazooooo <cest93@gmail.com>
2018-04-02 01:08:06 +0900

    firebaseで発行したserviceAccountサービス アカウントを追加 gitから除外 JSONファイルだとtsでimportしにくいのでtsモジュールにしておく

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,10 @@
-import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 import createUser from "./createUser";
+import serviceAccount from "./service_account";
 
-export {
-  createUser,
-};
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-db070.firebaseio.com",
+});
+
+export { createUser };
