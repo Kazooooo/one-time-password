0a816516cf7ae03e0f1d5d28f86d0325d4af4665
Kazooooo <cest93@gmail.com>
2018-04-02 23:07:35 +0900

    電話番号をlibphonenumberを使って国コード付きの番号に正規化 prittier適用

--- functions/src/createUser.ts
+++ functions/src/createUser.ts
@@ -1,5 +1,8 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+import * as libphonenumber from "google-libphonenumber";
+
+const phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();;
 
 const createUser = functions.https.onRequest((req, res) => {
   // Verify the user provided a phone
@@ -7,10 +10,13 @@ const createUser = functions.https.onRequest((req, res) => {
     res.status(422).send({ error: "Bad Input" });
   } else {
     // Format the phone number to remove dashes and parens
-    const phone = String(req.body.phone).replace(/[^\d]/g, "");
+    const number = phoneUtil.parseAndKeepRawInput(String(req.body.phone), "JP");
+    const phone = phoneUtil.format(number, libphonenumber.PhoneNumberFormat.E164);
 
     // Create a new account using that phone number
-    admin.auth().createUser({ uid: phone })
+    admin
+      .auth()
+      .createUser({ uid: phone })
       .then((user) => res.send(user))
       .catch((err) => res.status(422).send({ error: err }));
 
